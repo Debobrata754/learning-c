157cabe634f5df3aba99d36fba24b7ee246ff820
Debobrata Paul <debobratap076@gmail.com>
2025-08-14 18:05:31 +0530

    feat: new if condition added

--- hello-world.c
+++ hello-world.c
@@ -2,5 +2,9 @@
 
 int main() {
     printf("Hello, World!\n"); // Print message to console
+
+    if (3 > 2) {
+        printf("Hello, World 3 > 2!\n");
+    }
     return 0; // Indicate successful execution
 }
