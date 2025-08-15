e3bae7e169a4de190d4081ea6033fc4e13006e39
Debobrata Paul <debobratap076@gmail.com>
2025-08-15 17:35:51 +0530

    Command text file added

--- hello-world.c
+++ hello-world.c
@@ -1,10 +1,16 @@
 #include <stdio.h> // Standard input-output library
 
 int main() {
+    int a,b=10;
+    printf("Enter a number:");
+    scanf("%d",&a); // how to generate integer varialbe
     printf("Hello, World!\n"); // Print message to console
-
-    if (3 > 2) {
-        printf("Hello, World 3 > 2!\n");
+    if(a<20){
+        printf("%d\n",a+b);
+    }
+    else{
+        printf("%d\n",a-b);
     }
     return 0; // Indicate successful execution
 }
+//Video link:https://www.youtube.com/watch?v=irqbmMNs2Bo
