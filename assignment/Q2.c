5a3923d5da12b6a21c6d2de994a5634c22bee3ec
Debobrata Paul <debobratap076@gmail.com>
2025-09-16 19:04:31 +0530

    Removed repeated files.

---
+++ assignment/Q2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    printf("Enter two number : ");
+    scanf("%d %d",&a,&b);
+    sum = a + b;
+    printf("Sum of %d and %d is %d\n",a,b,sum);
+}
---
+++ assignment/Q9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter three numbers : ");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a > b && a > c){
+        printf("%d is greater than %d and %d\n",a,b,c);
+    }
+    else if(b > a && b > c){
+        printf("%d is greater than %d and %d\n",b,a,c);
+    }
+    else if(c > a && c > b){
+        printf("%d is greater than %d and %d\n",c,a,b);
+    }
+    return 0;
+}
